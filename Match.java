d1c46cd557723a9b9c0c6a235e37158df3a3987d
Marco <marco.mangan@gmail.com>
2025-08-27 21:34:02 +0000

    #2

--- Match.java
+++ Match.java
@@ -1,4 +1,10 @@
+/**
+ * Class Match represent the results of a soccer match.
+ * 
+ */
 class Match {
+
+    private String date;
 
     private String teamA;
     private String teamB;
@@ -6,6 +12,32 @@ class Match {
     private int goalsA;
     private int goalsB;
 
+    /**
+     * Create a 
+     * @param date
+     * @param teamA
+     * @param goalsA
+     * @param teamB
+     * @param goalsB
+     */
+    public Match(String date, String teamA, int goalsA, String teamB, int goalsB) {
+        // TODO: check for null
+        // TODO: check for negative numbers        
+        this.date = date;
 
+        this.teamA = teamA;
+        this.teamB = teamB;
+
+        this.goalsA = goalsA;
+        this.goalsB = goalsB;
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public String toString() {
+        return String.format("%s %s %d x %s %d", date, teamA, goalsA, teamB, goalsB);
+    }
 
 }
